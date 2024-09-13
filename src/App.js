70b4717b44234e7ffa714f65e61f2e4f38e1eb42
Felipe Silva <Taxistaloko14@outlook.com>
2024-09-13 16:26:31 -0300

    fix:colors-fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+    const handleToggle = () => {
+        setIsActive(!isActive);
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>StockFlow</h1>
+        <nav className='nav'>
+          <ul className={`nav-menu ${isActive ? 'active' : ''}`}>
+            <li className='nav-item'>
+              <a href='#inicio'>Inicio</a>
+            </li>
+            <li className='nav-item'>
+              <a href='#clinica'>Clinica</a>
+            </li>
+            <li className='nav-item'>
+              <a href='#farmacia'>Farmacia</a>
+            </li>
+            <li className='nav-item'>
+              <a href='#duvidas'>Dúvidas</a>
+            </li>
+          </ul>
+          <div className={`burger ${isActive ? 'active' : ''}`} onClick={handleToggle}>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+          </div>
+        </nav>
       </header>
+      <div className={`bg ${isActive ? 'active' : ''}`} onClick={handleToggle}>
+      </div>
     </div>
   );
 }
